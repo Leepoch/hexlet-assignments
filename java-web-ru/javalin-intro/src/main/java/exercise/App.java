2ee40b37b16389171891ba27443cde3e518f7662
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-06-20 15:57:10 +0300

    create project

---
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -0,0 +1,25 @@
+package exercise;
+import io.javalin.Javalin;
+
+// BEGIN
+
+// END
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        // BEGIN
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        // Описываем, что загрузится по адресу /
+        app.get("/welcome", ctx -> ctx.result("Welcome to Hexlet!"));
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
