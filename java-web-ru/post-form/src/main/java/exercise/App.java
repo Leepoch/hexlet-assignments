4fe9f63626309664bd0c9805a3270399332d269d
@hexlet/cli <support@hexlet.io>
2024-06-25 14:11:09 +0300

    submit java-web-ru/post-form

---
+++ java-web-ru/post-form/src/main/java/exercise/App.java
@@ -0,0 +1,58 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import io.javalin.rendering.template.JavalinJte;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import exercise.repository.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/users", ctx -> {
+            List<User> users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            ctx.render("users/index.jte", model("page", page));
+        });
+
+        // BEGIN
+        app.get("/users/build", ctx -> {
+            ctx.render("users/build.jte");
+        });
+
+        app.post("/users", ctx -> {
+           var firstName = ctx.formParam("firstName");
+           var lastName = ctx.formParam("lastName");
+           var email = ctx.formParam("email").trim().toLowerCase();
+           var password = ctx.formParam("password");
+           var capFirstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+           var capLastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
+
+           var user = new User(capFirstName, capLastName, email, Security.encrypt(password));
+           UserRepository.save(user);
+           ctx.redirect("/users");
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
